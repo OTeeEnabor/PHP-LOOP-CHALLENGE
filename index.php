cb82641ab36126ccc4ff4f293ff9bfb9f4990286
OTeeEnabor <tobienabor@gmail.com>
2019-08-21 21:02:52 +0200

    created the index.pp page for this loop callenge.

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Challenge 2:
+For this Challenge you are going to make a group of people with different attributes.
+Firstly make 4 arrays of 5 items each.
+The first array should contain names, the second hair colours, the third age and the fourth height.
+Then using loops you're going to create 5 people out of those arrays, each person taking one attribute from each. Each person should be a new array.
+Then you are going to add those 5 people arrays to a larger array called 'class'
+Lastly loop through 'class' and output each person's name followed by their other attributes with each person on a new line.
+*/
+
+$name = array('Jody','Eugene','Yusuf','Ola','Abdi');
+$color = array('orange','brown','blue','blonde','black');
+$age = array(20,25,20,22,23);
+$height = array('187cm','198cm','189cm','188cm','187cm');
+
+
+?>
